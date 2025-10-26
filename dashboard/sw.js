82e9c67fe1b460cfd1c1bb376067865459782739
manfredojuanignacio08-prog <manfredojuanignacio08@gmail.com>
2025-10-26 14:06:47 -0300

    Delete dashboard directory

--- dashboard/sw.js
+++
@@ -1,9 +0,0 @@
-const CACHE='ecosmart-v2';
-const ASSETS=['./','./index.html','./styles.css','./app.js','./logo.png','./manifest.webmanifest'];
-self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))))});
-self.addEventListener('fetch',e=>{
-  const u=new URL(e.request.url);
-  if(u.origin===location.origin){e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));}
-  else{e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)));}
-});
